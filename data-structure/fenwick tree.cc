34ed8c36cd08d5aa2bf7836e174376a3a434009b
morganliang <lzyrapx@gmail.com>
2026-06-28 01:56:19 +0800

    feat: add kth to fenwick tree

--- data-structure/fenwick tree.cc
+++ data-structure/fenwick tree.cc
@@ -14,4 +14,15 @@ struct BIT {
     for (; i > 0; i -= i & -i) s += tree[i];
     return s;
   }
+  int find_kth(T k) {
+    int id = 0, p2 = 1;
+    while (p2 * 2 <= n) p2 *= 2;
+    for (int i = p2; i > 0; i >>= 1) {
+      if (id + i <= n && tree[id + i] < k) {
+        id += i;
+        k -= tree[id];
+      }
+    }
+    return id + 1;
+  }
 };
